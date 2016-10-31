5c666f7a9329ef716de76f6731c2f1b63f0240e1
Booligoosh <booligoosh@optusnet.com.au>
2016-10-31 13:10:16 +1100

    removed solves problem fix

--- script.js
+++ script.js
@@ -353,9 +353,7 @@ function updateStorage() {
     if (typeof(Storage) !== "undefined") {
       localStorage.rubiksInspectionStartOn = inspectionStartOn;
       localStorage.rubiksBackgroundNum = backgroundNum;
-      if (solves.length >= 1) {
-        localStorage.rubiksSolves = solves.join();
-      }
+      localStorage.rubiksSolves = solves.join();
     } else {
       document.cookie = "cookies= |" + inspectionStartOn + "|" + backgroundNum + "|" + solves.join();
     }
